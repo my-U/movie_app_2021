5376fb0971b9226ab4a5ae873ddc9c5327ec9911
my-U <myouth_u@naver.com>
2021-10-03 16:58:16 +0900

    동적 Component 만들기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
 
-function Food({ fav }){ // React가 Food Component로 보내진 모든 정보를 가져온다. props.fav == { fav }
-  return <h1>I like {fav}</h1>
+function Food({ name, picture }){ 
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
+  )
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Food fav="kimchi" /> {/* food component에 fav라는 이름의 property를 kimchi라는 value로 보냄 */}
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukummi" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} /> )} {/*dish는 foodILike의 각 object임*/}
     </div>
   );
 }
