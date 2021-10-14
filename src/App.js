6805307c95282040e64cf85219b6ced98a409945
my-U <myouth_u@naver.com>
2021-10-14 21:52:39 +0900

    Style Css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
+
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); // axios로부터 movie list를 가져옴
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    ); // axios로부터 movie list를 가져옴
     this.setState({ movies, isLoading: false }); // movies : movies 이지만 movies로 생략 가능
-  }
-  componentDidMount(){ // 마운트가 끝나면 getMovies function 실행
+  };
+
+  componentDidMount() {
+    // 마운트가 끝나면 getMovies function 실행
     this.getMovies();
   }
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => (
-      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
-    ))}</div>;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
