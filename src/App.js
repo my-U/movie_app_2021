dfc152a4950ec803cd0bd698bf1e0b1067317de6
my-U <myouth_u@naver.com>
2021-11-01 23:46:32 +0900

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import About from "./routes/About";
 import Home from "./routes/Home";
 
 function App() {
   return (
-    <HashRouter>
-      <Route path="/" exact={true} component={Home} />{" "}
-      {/*exact는 화면에 컴포넌트가 2개 이상 랜더링 되는 것을 방지해줌*/}
-      <Route path="/about" component={About} />
-      {/*  /about과 /about/hello가 동시에 랜더링 됨
-      <Route path="/about/hello">
-        <h1>hello</h1>
-      </Route>*/}
-    </HashRouter>
+    <>
+      <HashRouter>
+        <Navigation /> {/* Router 밖에서는 사용 불가 */}
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </HashRouter>
+      <footer></footer>
+    </>
   );
 }
 export default App;
